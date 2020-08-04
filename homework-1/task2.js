157e858cdeeb4e5248bce6da96c3dc171aa31fc0
Denis Sushchenja <denis.sushchenja@gmail.com>
2020-08-04 21:53:07 +0300

    feat: implement homework-1 task2

---
+++ homework-1/task2.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+
+const txtDirPath = "./homework-1/txt";
+const csvPath = "homework-1/csv/nodejs-hw1.csv";
+const txtPath = `${txtDirPath}/nodejs-hw1.txt`;
+
+if (fs.existsSync(txtDirPath)) {
+	writeCsvToTxt(csvPath, txtPath);
+} else {
+	fs.mkdir(txtDirPath, { recursive: true }, (err) => {
+		if (err) {
+			throw err;
+		}
+		writeCsvToTxt(csvPath, txtPath);
+	});
+}
+
+function writeCsvToTxt(csvPath, txtPath) {
+	const readCsvStream = fs.createReadStream(csvPath);
+	const writeTxtStream = fs.createWriteStream(txtPath);
+	readCsvStream.on("error", (err) => console.log(err));
+	writeTxtStream.on("error", (err) => console.log(err));
+
+	readCsvStream
+		.pipe(
+			csv({
+				headers: ["book", "author", "amount", "price"],
+				colParser: {
+					price: "number",
+					amount: "omit",
+				},
+			})
+		)
+		.pipe(writeTxtStream);
+}
