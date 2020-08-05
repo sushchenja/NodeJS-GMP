f59397f6867047e8084244d8979a8c0dd1692ad0
Denis Sushchenja <denis.sushchenja@gmail.com>
2020-08-05 18:25:22 +0300

    fix: use 'data' event for the homework-1 task1

--- homework-1/task1.js
+++ homework-1/task1.js
@@ -1,11 +1,5 @@
-process.stdin.setEncoding("utf-8");
-
-process.stdin.on("readable", () => {
-	let chunk;
-	while (null !== (chunk = process.stdin.read())) {
-		process.stdout.write(`${reverseInputText(chunk)}\n`);
-	}
+process.stdin.on("data", (chunk) => {
+	process.stdout.write(`${chunk.reverse().toString()}\n`);
 });
-process.stdout.write("Enter string to transform:\n");
 
-const reverseInputText = (text) => text.split("").reverse().join("");
+process.stdout.write("Enter string to transform:\n");
